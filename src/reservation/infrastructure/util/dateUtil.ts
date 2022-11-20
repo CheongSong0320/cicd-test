86265df52c2e773d41bb7890797cf1181e2bda15
y0on2q <ltnscp9028@gmail.com>
2022-11-21 01:01:33 +0900

    Fix: utc in user get /reservation

--- src/reservation/infrastructure/util/dateUtil.ts
+++ src/reservation/infrastructure/util/dateUtil.ts
@@ -2,14 +2,16 @@ import * as dayjs from 'dayjs';
 import e from 'express';
 
 export const getDayCalculas = (day: number) => {
-  return dayjs()
-    .hour(0)
-    .minute(0)
-    .second(0)
-    .millisecond(0)
-    .add(day, 'day')
-    .add(9, 'hour')
-    .toDate();
+  return (
+    dayjs()
+      .hour(0)
+      .minute(0)
+      .second(0)
+      .millisecond(0)
+      .add(day, 'day')
+      // .add(9, 'hour')
+      .toDate()
+  );
 };
 
 export const calculateUsageMinute = (start: Date, end: Date) =>
