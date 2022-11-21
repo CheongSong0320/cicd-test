33a87a675edfbdbdd7fb00700e6fd935e6d31f03
y0on2q <ltnscp9028@gmail.com>
2022-11-22 01:39:41 +0900

    Implement: getCommunityUsageStatusDetail filter dateFrom,to

--- src/reservation/interface/getCommunityUsageStatusDetail.dto.ts
+++ src/reservation/interface/getCommunityUsageStatusDetail.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class GetCommunityUsageStatusDetailQuery {
+  @ApiProperty()
   dong: string;
+
+  @ApiProperty()
   ho: string;
+
+  @ApiPropertyOptional()
   dateFrom?: string;
+
+  @ApiPropertyOptional()
   dateTo?: string;
 }
